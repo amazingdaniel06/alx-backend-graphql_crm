0c6d4ec03d2db9ee24f9cac1947f245dc060a5b7
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-08-17 21:54:06 +0000

    Update settings.py

--- alx_backend_graphql/settings.py
+++ alx_backend_graphql/settings.py
@@ -1,1 +1,21 @@
+INSTALLED_APPS = [
+    # Django default apps...
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # Third-party
+    "graphene_django",
+    "django_filters",
+
+    # Local
+    "crm",
+]
+
+GRAPHENE = {
+    "SCHEMA": "alx_backend_graphql_crm.schema.schema"
+}
 

e2e49871981d318bc482a9555a947e5f193c2029
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-08-31 16:34:28 +0000

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -3,3 +3,15 @@ INSTALLED_APPS += ["django_crontab"]
 CRONJOBS = [
     ("*/5 * * * *", "crm.cron.log_crm_heartbeat"),
 ]
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+
+CELERY_BEAT_SCHEDULE = {
+    "generate-crm-report": {
+        "task": "crm.tasks.generate_crm_report",
+        "schedule": {
+            "day_of_week": "mon",
+            "hour": 6,
+            "minute": 0,
+        },
+    },
+}
